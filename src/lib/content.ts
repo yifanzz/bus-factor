87822a5377a38912f8e8904dc17902a7536e803f
yifanzz <4303312+yifanzz@users.noreply.github.com>
2024-10-29 20:55:06 +0000

    doc: update bus factor description

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -1,7 +1,8 @@
 export const CONTENT = {
     BUS_FACTOR: {
         TITLE: "What is Bus Factor?",
-        DESCRIPTION: "The \"bus factor\" is the minimum number of team members that have to suddenly disappear from a project before the project stalls due to lack of knowledgeable or competent personnel.",
+        DESCRIPTION: "The \"bus factor\" is the minimum number of team members that have to suddenly disappear from a project before the project stalls due to lack of knowledgeable or competent personnel."
+            + "Ideally, this number should be as higher than 3.",
         SCALE: {
             RED: "Critical: Single point of failure",
             AMBER: "Concerning: Limited backup",
